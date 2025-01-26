b360a3b8abe2b8b762e008fd6678d9a49aadc305
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-26 14:31:09 +0300

    dapper CategoryRepository

--- RealEstate_Dapper_Api/Dtos/CategoryDtos/ResultCategoryDto.cs
+++ RealEstate_Dapper_Api/Dtos/CategoryDtos/ResultCategoryDto.cs
@@ -2,5 +2,8 @@
 {
     public class ResultCategoryDto
     {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool CategoryStatus { get; set; }
     }
 }
---
+++ RealEstate_Dapper_Api/Repositories/CategoryRepository/CategoryRespository.cs
@@ -0,0 +1,25 @@
+﻿using Dapper;
+using RealEstate_Dapper_Api.Dtos.CategoryDtos;
+using RealEstate_Dapper_Api.Models.DapperContext;
+
+namespace RealEstate_Dapper_Api.Repositories.CategoryRepository
+{
+    public class CategoryRespository : ICategoryRepository
+    {
+        private readonly Context _context;
+        public CategoryRespository(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ResultCategoryDto>> GetAllCategoryAsync()
+        {
+            string query = "Select * From Category";
+            using(var connection=_context.CreateConnection())
+            {
+                var values = await connection.QueryAsync<ResultCategoryDto>(query);
+                return values.ToList();
+            }
+        }
+    }
+}
