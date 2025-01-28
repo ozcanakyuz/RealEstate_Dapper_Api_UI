197ace37ce754ed2dc73d4d3967fcf8f04ec5de2
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-28 21:04:29 +0300

    _DefaultHomePageProductList Api Sql

--- RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductDto.cs
+++ RealEstate_Dapper_Api/Dtos/ProductDtos/ResultProductDto.cs
@@ -8,6 +8,8 @@
         public string City { get; set; }
         public string CoverImage { get; set; }
         public string District { get; set; }
+        public string Address { get; set; }
+        public string Type { get; set; }
         public int ProductCategory { get; set; }
     }
 }
