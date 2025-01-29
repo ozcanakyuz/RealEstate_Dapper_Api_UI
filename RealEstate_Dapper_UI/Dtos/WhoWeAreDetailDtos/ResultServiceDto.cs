936890492e432d7c9edbd83546cf3e3bd90090c0
OZCAN AKYUZ <119737546+ozcanakyuz@users.noreply.github.com>
2025-01-29 16:06:48 +0300

    WhoWeAreDetailDto

---
+++ RealEstate_Dapper_UI/Dtos/WhoWeAreDetailDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RealEstate_Dapper_UI.Dtos.WhoWeAreDetailDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceId { get; set; }
+        public string ServiceName { get; set; }
+        public bool ServiceStatus { get; set; }
+
+    }
+}
